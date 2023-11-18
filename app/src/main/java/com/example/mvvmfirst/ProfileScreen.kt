bff0bf1275d7e542e638e3e69e8513c1f60c6ac8
Aruna Randika <randikaroxxs98@gmail.com>
2023-11-19 01:17:59 +0530

    profile info added.

--- app/src/main/java/com/example/mvvmfirst/ProfileScreen.kt
+++ app/src/main/java/com/example/mvvmfirst/ProfileScreen.kt
@@ -1,6 +1,8 @@
 package com.example.mvvmfirst
 
-import android.widget.Space
+import android.graphics.drawable.Icon
+import android.view.RoundedCorner
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -9,19 +11,28 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -39,6 +50,18 @@ fun ProfileScreen() {
         Spacer(
             modifier = Modifier.fillMaxWidth()
         )
+
+        ProfileSection(
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        ProfileInfo(
+            modifier = Modifier
+                .padding(10.dp)
+        )
+
+        ActionButtons()
+
 
     }
 
@@ -91,17 +114,28 @@ fun TopBar(
 fun ProfileSection(
     modifier: Modifier = Modifier
 ) {
-
     Column(
         modifier = modifier
     ) {
-
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 20.dp)
         ) {
+
+            RoundImage(
+                image = painterResource(id = R.drawable.randika),
+                modifier = Modifier
+                    .size(100.dp)
+                    .weight(3f)
+
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+
+            ProfileStat(
+                modifier = Modifier.weight(7f)
+            )
 
         }
     }
@@ -114,7 +148,6 @@ fun RoundImage(
     image: Painter,
     modifier: Modifier = Modifier
 ) {
-
     Image(
         painter = image,
         contentDescription = null,
@@ -131,10 +164,206 @@ fun RoundImage(
 
 }
 
+@Composable
+fun ProfileStat(
+    modifier: Modifier = Modifier
+) {
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier
+    ) {
+        Stats(
+            text = "232",
+            number = "Posts"
+        )
+        Stats(
+            text = "10.3k",
+            number = "Followers"
+        )
+        Stats(
+            text = "72",
+            number = "Following"
+        )
+
+    }
+
+}
+
+@Composable
+fun Stats(
+    text: String,
+    number: String,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+
+    ) {
+        Text(
+            text = text,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        Spacer(
+            modifier = Modifier
+                .height(4.dp)
+        )
+        Text(text = number)
+
+
+    }
+
+}
+
+@Composable
+fun ProfileInfo(
+    modifier: Modifier = Modifier
+
+) {
+
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "Aruna Randika",
+            fontWeight = FontWeight.Bold,
+            fontSize = 15.sp
+        )
+        Text(
+            text = "Personal blog",
+            fontSize = 15.sp,
+            color = Color.LightGray
+        )
+        HyperLinkView(link = "https://www.instagram.com/aruna.randika/")
+
+        Text(text = "Aruna Randika")
+
+        FollowersInfo()
+
+    }
+
+}
+
+
+@Composable
+fun HyperLinkView(
+    link: String,
+    modifier: Modifier = Modifier
+
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            modifier = Modifier.size(15.dp),
+            painter = painterResource(id = R.drawable.link),
+            contentDescription = "link to "
+        )
+        Text(
+            modifier = Modifier
+                .padding(start = 5.dp),
+            text = link,
+            color = Color.Blue
+
+        )
+    }
+
+}
+
+@Composable
+fun FollowersInfo(
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RoundImage(
+            image = painterResource(id = R.drawable.philipp),
+            modifier = Modifier
+                .size(30.dp)
+        )
+
+        Text(
+            text = "Followed by",
+        )
+        Text(
+            modifier = Modifier.padding(start = 3.dp),
+            text = "Philipp Lackner",
+            fontWeight = FontWeight.Bold
+        )
+
+    }
+}
+
+@Composable
+fun ActionButtons() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        TextButton(
+            onClick = { },
+            modifier = Modifier
+                .padding(5.dp)
+                .weight(4f),
+            colors = ButtonDefaults.textButtonColors(Color.Gray),
+            shape = ButtonDefaults.outlinedShape
+        ) {
+            Text(
+                "Following",
+                color = Color.White
+            )
+        }
+        TextButton(
+            onClick = { },
+            modifier = Modifier
+                .padding(5.dp)
+                .weight(4f),
+            colors = ButtonDefaults.textButtonColors(Color.Gray),
+            shape = ButtonDefaults.outlinedShape
+        ) {
+            Text(
+                "Message",
+                color = Color.White
+            )
+        }
+        TextButton(
+            onClick = { },
+            modifier = Modifier
+                .padding(5.dp)
+                .weight(1f),
+            colors = ButtonDefaults.textButtonColors(Color.Gray),
+            shape = ButtonDefaults.outlinedShape
+        ) {
+            Icon(
+                modifier = Modifier.size(15.dp),
+                painter = painterResource(id = R.drawable.add_friend),
+                contentDescription = "add friend ",
+            )
+        }
+
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewTheUI() {
-    RoundImage(
-        image = painterResource(id = R.drawable.philipp)
-    )
+
+    ProfileScreen()
+
+
 }
